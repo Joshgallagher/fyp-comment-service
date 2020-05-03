da4effa48a3c474f6dc3f8a4ee9a7250e890d928
Josh <joshgallagher24@gmail.com>
2020-05-03 21:09:39 +0100

    Created Comment Model

---
+++ src/comment/model/comment.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from src.core.database import database
+
+
+class Rating(database.Document):
+    user_id = database.UUIDField(binary=False, required=True)
+    article_id = database.IntField(required=True)
+    comment = database.StringField()
+    created_at = database.DateTimeField(
+        required=True, default=datetime.now())
+    updated_at = database.DateTimeField(
+        required=True, default=datetime.now())
