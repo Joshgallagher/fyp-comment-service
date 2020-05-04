1dbbc329794944f538b2c5b91959db087925eed2
Josh <joshgallagher24@gmail.com>
2020-05-04 06:37:40 +0100

    Implemented article comments endpoint with tests

--- src/comment/resource/article_comments_resource.py
+++ src/comment/resource/article_comments_resource.py
@@ -1,6 +1,26 @@
 from flask_restful import Resource
+from src.comment.model.comment import Comment
 
 
 class ArticleCommentsResource(Resource):
     def get(self, id):
-        return {}, 200
+        pipeline = [
+            {
+                '$project': {
+                    '_id': 0,
+                    'id': {
+                        '$toString': '$_id'
+                    },
+                    'userId': '$user_id',
+                    'comment': 1,
+                    'createdAt': {
+                        '$dateToString': {
+                            'date': '$created_at',
+                        }
+                    }
+                }
+            }
+        ]
+        comments = Comment.objects(article_id=id).aggregate(pipeline)
+
+        return list(comments), 200
